69ca22a626f6e444f6af9220be7f2877b60c775f
rontysarkar <rontysarkar2023@gmail.com>
2024-05-14 13:23:44 +0600

    complate website

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://cafe-rain.web.app",
+
+    ],
     credentials: true,
   })
 );
